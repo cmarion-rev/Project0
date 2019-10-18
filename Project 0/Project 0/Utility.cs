e63b9caa3868b7fabd9431edbe2fe96aa32e782f
Cameron Marion <cameron.marion@revature.net>
2019-10-18 15:23:06 -0400

    Created new Utility class. Added OperateState enum to Utility class for tracking which selection has been made.

---
+++ Project 0/Project 0/Utility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    class Utility
+    {
+        public enum OperationState
+        {
+            REGISTER,
+            OPEN_ACCOUNT,
+            CLOSE_ACCOUNT,
+            WITHDRAW,
+            DEPOSIT,
+            TRANSFER,
+            PAY_LOAN,
+            DISPLAY_ACCOUNTS,
+            DISPLAY_TRANSACTIONS,
+        }
+
+    }
+}
