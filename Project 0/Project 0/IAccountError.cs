b97ac688b229745c3aa9cfafdbd2fc0454573b26
Cameron Marion <cameron.marion@revature.net>
2019-10-18 19:20:26 -0400

    Moved transaction error states into new interface.

---
+++ Project 0/Project 0/IAccountError.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    public interface IAccountError
+    {
+        public Utility.TransactionErrorCodes LastTransactionState { get; }
+    }
+}
