d3a6a16ec7e2c99297a45553b509383c87598b9d
Cameron Marion <cameron.marion@revature.net>
2019-10-18 19:42:36 -0400

    Added functionality and condition checks for proper amount and MaturityDate to TermDepositAccount class WithdrawAmount.

--- Project 0/Project 0/TermDepositAccount.cs
+++ Project 0/Project 0/TermDepositAccount.cs
@@ -6,15 +6,18 @@ namespace Project_0
 {
     public class TermDepositAccount : Account, IAccountInfo
     {
-        public DateTime MaturityDate { get; set; }
-
         public TermDepositAccount(Customer newCustomer) : base()
         {
             AccountNumber = IAccountInfo.totalAccounts++;
             Customer = newCustomer;
             // CustomerID = newCustomer.CustomerID;
             AccountBalance = 0.0;
+
+            // Default 1 Year term.
+            MaturityDate = DateTime.Now.AddYears(1);
         }
+
+        public DateTime MaturityDate { get; set; }
 
         public int CustomerID { get; set; }
 
@@ -31,12 +34,15 @@ namespace Project_0
             bool result = true;
             LastTransactionState = Utility.TransactionErrorCodes.SUCCESS;
 
+            // Check if amount selected is a valid number.
             if (newAmount > 0.0f)
             {
+                AccountBalance += newAmount;
                 totalRecords.Add(new DepositRecord() { TransactionAmount = newAmount, TransactionCode = Utility.TransactionErrorCodes.SUCCESS });
             }
             else
             {
+                // Invalid amount selected.
                 result = false;
                 LastTransactionState = Utility.TransactionErrorCodes.INVALID_AMOUNT;
             }
@@ -46,17 +52,61 @@ namespace Project_0
 
         public bool WithdrawAmount(double newAmount)
         {
-            throw new NotImplementedException();
+            bool result = true;
+            LastTransactionState = Utility.TransactionErrorCodes.SUCCESS;
+
+            // Check if amount selected is a valid number.
+            if (newAmount > 0.0)
+            {
+                // Check if maturity date has passed.
+                result = CheckMaturity(newAmount);
+            }
+            else
+            {
+                // Invalid amount selected.
+                result = false;
+                LastTransactionState = Utility.TransactionErrorCodes.INVALID_AMOUNT;
+            }
+
+            return result;
         }
 
-        bool IDeposit.DepositAmount(double newAmount)
+        private bool CheckMaturity(double newAmount)
         {
-            throw new NotImplementedException();
+            bool result = true;
+            
+            if (MaturityDate.Date.Subtract(DateTime.Now).Days > -1)
+            {
+                // Check if withdraw amount does not exceed current account amount.
+                result = CheckOverdrafting(newAmount);
+            }
+            else
+            {
+                // Maturity not reached.
+                result = false;
+                LastTransactionState = Utility.TransactionErrorCodes.TERM_PROTECTION;
+            }
+
+            return result;
         }
 
-        bool IWithdrawal.WithdrawAmount(double newAmount)
+        private bool CheckOverdrafting(double newAmount)
         {
-            throw new NotImplementedException();
+            bool result = true;
+            
+            if (newAmount <= AccountBalance)
+            {
+                AccountBalance -= newAmount;
+                totalRecords.Add(new WithdrawalRecord() { TransactionAmount = newAmount, TransactionCode = Utility.TransactionErrorCodes.SUCCESS });
+            }
+            else
+            {
+                // Maturity not reached.
+                result = false;
+                LastTransactionState = Utility.TransactionErrorCodes.OVERDRAFT;
+            }
+
+            return result;
         }
     }
 }
