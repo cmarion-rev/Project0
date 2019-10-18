66114d714ce8063f7eadfcabcc6a5562ead29de4
Cameron Marion <cameron.marion@revature.net>
2019-10-18 15:43:24 -0400

    Created new interface for account withdrawing.

---
+++ Project 0/Project 0/IWithdrawal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface IWithdrawal
+    {
+        public Utility.WithdrawalErrorCodes WithdrawState { get; }
+
+        public bool WithdrawAmount(double newAmount);
+
+        public Utility.WithdrawalErrorCodes LastWithdrawError();
+    }
+}
