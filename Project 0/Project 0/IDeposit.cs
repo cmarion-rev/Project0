2eea15c8493ccd4b2038e11a7d372722742e0db0
Cameron Marion <cameron.marion@revature.net>
2019-10-18 15:45:00 -0400

    Created new interface for account depositing.

---
+++ Project 0/Project 0/IDeposit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface IDeposit
+    {
+        public bool DepositAmount(double newAmount);
+    }
+}
