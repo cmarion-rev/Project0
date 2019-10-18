c7de3620fe8b001ec87dc391273a22172a482a40
Cameron Marion <cameron.marion@revature.net>
2019-10-18 19:21:02 -0400

    Started building TermDepositAccount class, integrating all relevant interfaces and base Account class.

--- Project 0/Project 0/TermDepositAccount.cs
+++ Project 0/Project 0/TermDepositAccount.cs
@@ -4,7 +4,58 @@ using System.Text;
 
 namespace Project_0
 {
-    class TermDepositAccount : Account, IAccountInfo
+    public class TermDepositAccount : Account, IAccountInfo
     {
+
+        public TermDepositAccount(Customer newCustomer) : base()
+        {
+            AccountNumber = IAccountInfo.totalAccounts++;
+            Customer = newCustomer;
+            // CustomerID = newCustomer.CustomerID;
+            AccountBalance = 0.0;
+        }
+
+        public int CustomerID { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public double AccountBalance { get; private set; }
+
+        public Utility.TransactionErrorCodes LastTransactionState { get; private set; }
+
+        public int AccountNumber { get; }
+
+        public bool DepositAmount(double newAmount)
+        {
+            bool result = true;
+            LastTransactionState = Utility.TransactionErrorCodes.SUCCESS;
+
+            if (newAmount > 0.0f)
+            {
+
+            
+            }
+            else
+            {
+
+            }
+
+            return result;
+        }
+
+        public bool WithdrawAmount(double newAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IDeposit.DepositAmount(double newAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IWithdrawal.WithdrawAmount(double newAmount)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
