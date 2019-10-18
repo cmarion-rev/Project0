dcf40344f038f057c025e6d0a61f2caf997a3f1d
Cameron Marion <cameron.marion@revature.net>
2019-10-18 18:12:53 -0400

    Implemented interface for WithdrawalRecord class.

--- Project 0/Project 0/WithdrawalRecord.cs
+++ Project 0/Project 0/WithdrawalRecord.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace Project_0
 {
-    class WithdrawalRecord:ITransactionRecord
+    class WithdrawalRecord : ITransactionRecord
     {
+        public WithdrawalRecord()
+        {
+            TransactionID = ITransactionRecord.TransactionNumber++;
+        }
+
+        public int TransactionID { get; }
+
+        public double TransactionAmount { get; set; }
+
+        public Utility.TransactionErrorCodes TransactionCode { get; set; }
     }
 }
