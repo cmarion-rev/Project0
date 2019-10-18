6d7d38af9944f93357f4dc304dfab83b694c396b
Cameron Marion <cameron.marion@revature.net>
2019-10-18 18:12:43 -0400

    Implemented interface for DepositRecord class.

--- Project 0/Project 0/DepositRecord.cs
+++ Project 0/Project 0/DepositRecord.cs
@@ -6,5 +6,15 @@ namespace Project_0
 {
     class DepositRecord : ITransactionRecord
     {
+        public DepositRecord()
+        {
+            TransactionID = ITransactionRecord.TransactionNumber++;
+        }
+
+        public int TransactionID { get; }
+
+        public double TransactionAmount { get; set; }
+
+        public Utility.TransactionErrorCodes TransactionCode { get; set; }
     }
 }
