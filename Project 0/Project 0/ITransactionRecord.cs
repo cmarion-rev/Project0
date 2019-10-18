989201812c446240deea389006940be38f6d3889
Cameron Marion <cameron.marion@revature.net>
2019-10-18 16:04:10 -0400

    Created new interface for recording transactions.

---
+++ Project 0/Project 0/ITransactionRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface ITransactionRecord
+    {
+        public static int TransactionNumber = 0;
+
+        public int TransactionID { get; set; }
+
+        public double TransactionAmount { get; set; }
+
+        public Utility.TransactionErrorCodes TransactionCode { get; }
+    }
+}
