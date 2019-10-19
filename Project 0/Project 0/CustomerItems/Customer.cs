6b03ac28099d5fe7008d7da5de7d0b2784cb0436
Cameron Marion <cameron.marion@revature.net>
2019-10-19 12:20:18 -0400

    Created initial setup for Customer class.

--- Project 0/Project 0/CustomerItems/Customer.cs
+++ Project 0/Project 0/CustomerItems/Customer.cs
@@ -6,5 +6,18 @@ namespace Project_0
 {
     public class Customer
     {
+        private static int totalCustomers = 0;
+
+        public int CustomerID { get;  }
+
+        private List<Account> accounts = new List<Account>();
+
+        Customer()
+        {
+            CustomerID = ++totalCustomers;
+            accounts = new List<Account>();
+        }
+
+
     }
 }
