3c0dcb5cd9d1cd282d6514247d3bfa5a88ac5c30
Cameron Marion <cameron.marion@revature.net>
2019-10-19 18:26:03 -0400

    Created interface for accessing accounts in AccountData class.

---
+++ Project 0/Project 0/Storage/Interfaces/IAccountDataGetAccounts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface IAccountDataGetAccounts
+    {
+        public Account GetAccount(int accountNumber);
+                    
+        public CheckingAccount GetCheckingAccount(int accountNumber);
+                    
+        public BusinessAccount GetBusinessAccount(int accountNumber);
+                    
+        public TermDepositAccount GetTermAccount(int accountNumber);
+                    
+        public LoanAccount GetLoanAccount(int accountNumber);
+    }
+}
