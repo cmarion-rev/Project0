b13b887f5bba41ad1745a6f22346ae07574a7816
Cameron Marion <cameron.marion@revature.net>
2019-10-18 20:54:38 -0400

    Setup initial implementation for BusinessAccount class.

--- Project 0/Project 0/AccountItems/BusinessAccount.cs
+++ Project 0/Project 0/AccountItems/BusinessAccount.cs
@@ -6,5 +6,37 @@ namespace Project_0
 {
     class BusinessAccount : Account, IAccountInfo
     {
+        public BusinessAccount(Customer newCustomer) : base()
+        {
+            AccountNumber = IAccountInfo.totalAccounts++;
+
+            // Set customer references.
+            Customer = newCustomer;
+            // CustomerID = newCustomer.CustomerID;
+
+            // Set initial account balance.
+            AccountBalance = 0.0;
+        }
+
+        public int AccountNumber { get; }
+
+        public int CustomerID { get ; set ; }
+        public Customer Customer { get ; set; }
+
+        public double AccountBalance { get; private set; }
+
+        public Utility.TransactionErrorCodes LastTransactionState { get; private set; }
+
+        public double OverdraftBalance { get; private set; }
+
+        public bool DepositAmount(double newAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WithdrawAmount(double newAmount)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
