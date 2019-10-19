cf99b4bb94c35f2c80635fcf9c7b7224fb1252f1
Cameron Marion <cameron.marion@revature.net>
2019-10-19 18:27:29 -0400

    Created initial functions for implementing Add, Get, and Remove interfaces for AccountData class.

--- Project 0/Project 0/Storage/AccountData.cs
+++ Project 0/Project 0/Storage/AccountData.cs
@@ -4,14 +4,86 @@ using System.Text;
 
 namespace Project_0
 {
-    class AccountData
+    class AccountData : IAccountDataAddAccounts, IAccountDataGetAccounts, IAccountDataRemoveAccounts
     {
         private List<CheckingAccount> allCheckingAccounts = new List<CheckingAccount>();
         private List<BusinessAccount> allBusinessAccounts = new List<BusinessAccount>();
         private List<TermDepositAccount> allTermAccounts = new List<TermDepositAccount>();
         private List<LoanAccount> allLoanAccounts = new List<LoanAccount>();
 
+        public bool AddAccount(Account newAccount)
+        {
+            throw new NotImplementedException();
+        }
 
+        public bool AddBusinessAccount(BusinessAccount newAccount)
+        {
+            throw new NotImplementedException();
+        }
 
+        public bool AddCheckingAccount(CheckingAccount newAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddLoanAccount(LoanAccount newAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddTermAccount(TermDepositAccount newAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account GetAccount(int accountNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BusinessAccount GetBusinessAccount(int accountNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CheckingAccount GetCheckingAccount(int accountNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LoanAccount GetLoanAccount(int accountNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TermDepositAccount GetTermAccount(int accountNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveAccount(Account newAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveBusinessAccount(BusinessAccount newAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveCheckingAccount(CheckingAccount newAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveLoanAccount(LoanAccount newAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveTermAccount(TermDepositAccount newAccount)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
