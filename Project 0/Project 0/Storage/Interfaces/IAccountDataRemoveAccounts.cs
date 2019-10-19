5a8812a4e7b5753dbdd90c1f01450e877b915bdf
Cameron Marion <cameron.marion@revature.net>
2019-10-19 18:21:29 -0400

    Created interface for removing accounts from AccountData class.

---
+++ Project 0/Project 0/Storage/Interfaces/IAccountDataRemoveAccounts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface IAccountDataRemoveAccounts
+    {
+        public bool RemoveAccount(Account newAccount);
+                    
+        public bool RemoveCheckingAccount(CheckingAccount newAccount);
+                    
+        public bool RemoveBusinessAccount(BusinessAccount newAccount);
+                    
+        public bool RemoveTermAccount(TermDepositAccount newAccount);
+                    
+        public bool RemoveLoanAccount(LoanAccount newAccount);
+    }
+}
