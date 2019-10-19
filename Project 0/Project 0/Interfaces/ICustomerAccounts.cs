9513dacd5350e6751b831c8c204b5c28bbd8c476
Cameron Marion <cameron.marion@revature.net>
2019-10-19 12:28:16 -0400

    Created new interface for accessing all customer accounts, derived from CustomerAccountUpdates interface.

---
+++ Project 0/Project 0/Interfaces/ICustomerAccounts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface ICustomerAccounts : ICustomerAccountUpdates
+    {
+        public List<Account> GetAccounts();
+        
+        public List<CheckingAccount> GetCheckingAccounts();
+        
+        public List<BusinessAccount> GetBusinessAccounts();
+        
+        public List<TermDepositAccount> GetTermDepositAccounts();
+        
+        public List<LoanAccount> GetLoanAccounts();
+
+        public Account GetAccount(int index);
+    }
+}
