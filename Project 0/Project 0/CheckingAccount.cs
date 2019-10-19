c71aaedfb3c70b5beb533627547941086e6b9574
Cameron Marion <cameron.marion@revature.net>
2019-10-18 20:26:26 -0400

    Setup initial interfacing for CheckingAccount class. Added static field for singular interest rate across all CheckingAccount classes.

--- Project 0/Project 0/CheckingAccount.cs
+++ Project 0/Project 0/CheckingAccount.cs
@@ -6,6 +6,38 @@ namespace Project_0
 {
     class CheckingAccount : Account, IAccountInfo
     {
+        public CheckingAccount(Customer newCustomer) : base()
+        {
+            AccountNumber = IAccountInfo.totalAccounts++;
 
+            // Set customer references.
+            Customer = newCustomer;
+            // CustomerID = newCustomer.CustomerID;
+
+            // Set initial account balance.
+            AccountBalance = 0.0;
+        }
+
+        public static double InterestRate { get; set; }
+
+        public int AccountNumber { get; }
+
+        public int CustomerID { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public double AccountBalance { get; private set; }
+
+        public Utility.TransactionErrorCodes LastTransactionState { get; private set; }
+
+        public bool DepositAmount(double newAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WithdrawAmount(double newAmount)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
