5324b7047793eda12d49fc8242bfdd0adaa80157
Cameron Marion <cameron.marion@revature.net>
2019-10-18 20:43:29 -0400

    Created functionality for DepositAmount in LoanAccount class.

--- Project 0/Project 0/LoanAccount.cs
+++ Project 0/Project 0/LoanAccount.cs
@@ -28,9 +28,30 @@ namespace Project_0
 
         public Utility.TransactionErrorCodes LastTransactionState { get; private set; }
 
+        /// <summary>
+        /// Deposits funds to account.
+        /// </summary>
+        /// <param name="newAmount">Amount to be deposited.</param>
+        /// <returns>Returns true if transaction is valid; Otherwise, false.</returns>
         public bool DepositAmount(double newAmount)
         {
-            throw new NotImplementedException();
+            bool result = true;
+            LastTransactionState = Utility.TransactionErrorCodes.SUCCESS;
+
+            // Check if amount selected is a valid number.
+            if (newAmount > 0.0f)
+            {
+                AccountBalance += newAmount;
+                totalRecords.Add(new DepositRecord() { TransactionAmount = newAmount, TransactionCode = Utility.TransactionErrorCodes.SUCCESS });
+            }
+            else
+            {
+                // Invalid amount selected.
+                result = false;
+                LastTransactionState = Utility.TransactionErrorCodes.INVALID_AMOUNT;
+            }
+
+            return result;
         }
 
         public bool WithdrawAmount(double newAmount)
