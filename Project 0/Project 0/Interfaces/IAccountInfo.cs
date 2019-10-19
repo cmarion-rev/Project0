f8b59c7a0c633e14f65a9fef57f41d6e1db2dc31
Cameron Marion <cameron.marion@revature.net>
2019-10-19 13:11:33 -0400

    Changed access control for totalAccounts in AccountInfo interface to private.

--- Project 0/Project 0/Interfaces/IAccountInfo.cs
+++ Project 0/Project 0/Interfaces/IAccountInfo.cs
@@ -6,7 +6,7 @@ namespace Project_0
 {
     public interface IAccountInfo : IDeposit, IWithdrawal, IAccountError
     {
-        protected static int totalAccounts = 0;
+        private static int totalAccounts = 0;
         protected static int GetNewAccountNumber()
         {
             return ++totalAccounts;
