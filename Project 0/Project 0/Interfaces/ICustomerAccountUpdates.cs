3d1abbc532c0fbc5805aeb46be625c7c86444352
Cameron Marion <cameron.marion@revature.net>
2019-10-19 12:23:51 -0400

    Created new interface for adding and removing accounts from customers.

---
+++ Project 0/Project 0/Interfaces/ICustomerAccountUpdates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface ICustomerAccountUpdates
+    {
+        public bool AddAccount(Account newAccount);
+
+
+        public bool RemoveAccount(Account newAccount);
+    }
+}
