9593c37c63401044f5e4ad9f6feda11b95cc9b4a
Cameron Marion <cameron.marion@revature.net>
2019-10-19 12:38:08 -0400

    Implemented functionality for Customer class get account functions.

--- Project 0/Project 0/CustomerItems/Customer.cs
+++ Project 0/Project 0/CustomerItems/Customer.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+
 
 namespace Project_0
 {
-    public class Customer
+    public class Customer : ICustomerAccounts
     {
         private static int totalCustomers = 0;
 
@@ -18,6 +20,65 @@ namespace Project_0
             accounts = new List<Account>();
         }
 
+        public List<Account> GetAccounts()
+        {
+            return accounts;
+        }
 
+        public List<CheckingAccount> GetCheckingAccounts()
+        {
+            List<CheckingAccount> result = new List<CheckingAccount>();
+
+            result = accounts.Where(account => account is CheckingAccount).ToList().Cast<CheckingAccount>().ToList();
+
+            return result;
+        }
+
+        public List<BusinessAccount> GetBusinessAccounts()
+        {
+            List<BusinessAccount> result = new List<BusinessAccount>();
+
+            result = accounts.Where(account => account is BusinessAccount).ToList().Cast<BusinessAccount>().ToList();
+
+            return result;
+        }
+
+        public List<TermDepositAccount> GetTermDepositAccounts()
+        {
+            List<TermDepositAccount> result = new List<TermDepositAccount>();
+
+            result = accounts.Where(account => account is TermDepositAccount).ToList().Cast<TermDepositAccount>().ToList();
+
+            return result;
+        }
+
+        public List<LoanAccount> GetLoanAccounts()
+        {
+            List<LoanAccount> result = new List<LoanAccount>();
+
+            result = accounts.Where(account => account is LoanAccount).ToList().Cast<LoanAccount>().ToList();
+
+            return result;
+        }
+
+        public Account GetAccount(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddAccount(Account newAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveAccount(Account newAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetNumberOfAccounts()
+        {
+            return accounts.Count;
+        }
     }
 }
