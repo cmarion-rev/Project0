aeaee177244f9fcd3c368d12d5f27598c8c084ad
Cameron Marion <cameron.marion@revature.net>
2019-10-19 12:57:03 -0400

    Created static method for creating new account numbers, inside AccountInfo interface.

--- Project 0/Project 0/Interfaces/IAccountInfo.cs
+++ Project 0/Project 0/Interfaces/IAccountInfo.cs
@@ -7,6 +7,10 @@ namespace Project_0
     public interface IAccountInfo : IDeposit, IWithdrawal, IAccountError
     {
         protected static int totalAccounts = 0;
+        protected static int GetNewAccountNumber()
+        {
+            return ++totalAccounts;
+        }
 
         public int AccountNumber { get; }
 
