ddf247b3d2e142ca73bceb568ef131462b051b2d
Cameron Marion <cameron.marion@revature.net>
2019-10-19 18:20:09 -0400

    Created new interface for adding accounts to AccountData class.

---
+++ Project 0/Project 0/Storage/Interfaces/IAccountDataAddAccounts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface IAccountDataAddAccounts
+    {
+        public bool AddAccount(Account newAccount);
+
+        public bool AddCheckingAccount(CheckingAccount newAccount);
+        
+        public bool AddBusinessAccount(BusinessAccount newAccount);
+        
+        public bool AddTermAccount(TermDepositAccount newAccount);
+        
+        public bool AddLoanAccount(LoanAccount newAccount);
+    }
+}
