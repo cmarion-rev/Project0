bf3a1cecbcf29b74a5f5a20e1f4057a1c17dde6b
Cameron Marion <cameron.marion@revature.net>
2019-10-19 13:14:47 -0400

    Created new interface for controlling customer information.

---
+++ Project 0/Project 0/Interfaces/ICustomerInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface ICustomerInformation
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get; }
+    }
+}
