2a24ad4a92f72b4d286e3d2c5e54c6be8b437b27
Cameron Marion <cameron.marion@revature.net>
2019-10-18 20:55:53 -0400

    Created functionality for DepositAmount in BusinessAccount class.

--- Project 0/Project 0/AccountItems/BusinessAccount.cs
+++ Project 0/Project 0/AccountItems/BusinessAccount.cs
@@ -29,9 +29,30 @@ namespace Project_0
 
         public double OverdraftBalance { get; private set; }
 
+        /// <summary>
+        /// Deposits funds to account.
+        /// </summary>
+        /// <param name="newAmount">Amount to be deposited.</param>
+        /// <returns>Returns true if transaction is valid; Otherwise, false.</returns>
         public bool DepositAmount(double newAmount)
         {
-            throw new NotImplementedException();
+            bool result = true;
+            LastTransactionState = Utility.TransactionErrorCodes.SUCCESS;
+
+            // Check if amount selected is a valid number.
+            if (newAmount > 0.0f)
+            {
+                AccountBalance += newAmount;
+                totalRecords.Add(new DepositRecord() { TransactionAmount = newAmount, TransactionCode = Utility.TransactionErrorCodes.SUCCESS });
+            }
+            else
+            {
+                // Invalid amount selected.
+                result = false;
+                LastTransactionState = Utility.TransactionErrorCodes.INVALID_AMOUNT;
+            }
+
+            return result;
         }
 
         public bool WithdrawAmount(double newAmount)
