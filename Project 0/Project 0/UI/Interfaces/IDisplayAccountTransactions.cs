d66ed886e82e268a143d090d85918eefdf7e6104
Cameron Marion <cameron.marion@revature.net>
2019-10-21 10:49:19 -0400

    Created new interface for displaying all transactions for specific account.

---
+++ Project 0/Project 0/UI/Interfaces/IDisplayAccountTransactions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface IDisplayAccountTransactions
+    {
+        public void DisplayAllAccountTransactions(ITransactionRecord[] allTransactions);
+    }
+}
