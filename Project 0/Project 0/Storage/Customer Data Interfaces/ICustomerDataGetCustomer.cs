e34cbf13cd460d3cc473f0a98337de85b480606e
Cameron Marion <cameron.marion@revature.net>
2019-10-21 09:13:26 -0400

    Created interface for getting singular or all customer(s) from Customer Data class.

---
+++ Project 0/Project 0/Storage/Customer Data Interfaces/ICustomerDataGetCustomer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface ICustomerDataGetCustomer
+    {
+        public Customer GetCustomer(int customerID);
+
+        public Customer[] GetAllCustomers();
+    }
+}
