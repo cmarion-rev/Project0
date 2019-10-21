df9312afa212d95e8c1bdc6101b129a45a02f60a
Cameron Marion <cameron.marion@revature.net>
2019-10-21 08:54:42 -0400

    Created initial setup functionality for CustomerData storage class. Created singleton style layout for CustomerData class.

--- Project 0/Project 0/Storage/CustomerData.cs
+++ Project 0/Project 0/Storage/CustomerData.cs
@@ -6,5 +6,25 @@ namespace Project_0
 {
     class CustomerData
     {
+        private readonly List<Customer> allCustomer = new List<Customer>();
+
+        private static CustomerData workingInstance = null;
+
+        public static CustomerData Instance
+        {
+            get
+            {
+                if (workingInstance == null)
+                {
+                    workingInstance = new CustomerData();
+                }
+                return workingInstance;
+            }
+        }
+
+        CustomerData()
+        {
+            allCustomer = new List<Customer>();
+        }
     }
 }
