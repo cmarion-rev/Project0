3ccccbbd720d51f3e002e985640314492a9fe55e
Cameron Marion <cameron.marion@revature.net>
2019-10-21 15:52:20 -0400

    Created new interface for display new account balance inquires.

---
+++ Project 0/Project 0/UI/Interfaces/Display Account Interfaces/IDisplayNewAccountOption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0.AccountInterfaces
+{
+    interface IDisplayNewAccountOption
+    {
+        public void DisplayNewCheckingAccountBalance();
+
+        public void DisplayNewBusinessAccountBalance();
+
+        public void DisplayNewLoanAccountBalance();
+
+        public void DisplayNewTermAccountBalance();
+    }
+}
