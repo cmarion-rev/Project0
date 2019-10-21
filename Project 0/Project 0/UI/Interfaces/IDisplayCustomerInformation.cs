c47e63f6247a3dbdc5415398b91d5a815838d0e5
Cameron Marion <cameron.marion@revature.net>
2019-10-21 10:45:18 -0400

    Created new interface for displaying customer information.

---
+++ Project 0/Project 0/UI/Interfaces/IDisplayCustomerInformation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0.UI.Interfaces
+{
+    interface IDisplayCustomerInformation
+    {
+        public void DisplayCustomerInformation(Customer newCustomer);
+    }
+}
