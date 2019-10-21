dea9454e2fe295779393ff9d1a2c85d10262e6be
Cameron Marion <cameron.marion@revature.net>
2019-10-21 11:13:35 -0400

    Created new interface for displaying loan account for paying installment to.

---
+++ Project 0/Project 0/UI/Interfaces/IDisplayAccountLoanPayment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface IDisplayAccountLoanPayment
+    {
+        public void DisplayLoanAccountSelection(Account[] allAccounts);
+
+        public void DisplayLoanInstallment();
+    }
+}
