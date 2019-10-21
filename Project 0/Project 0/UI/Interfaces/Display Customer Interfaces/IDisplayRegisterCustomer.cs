d38dc42ca0206a7e5c4779c88088e2b6a0d85576
Cameron Marion <cameron.marion@revature.net>
2019-10-21 14:43:22 -0400

    Created new display interface for requesting customer data on new customer registration.

---
+++ Project 0/Project 0/UI/Interfaces/Display Customer Interfaces/IDisplayRegisterCustomer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0.CustomerInterfaces
+{
+    interface IDisplayRegisterCustomer
+    {
+        public void DisplayCustomerFirstNameRequest(string newName = "");
+
+        public void DisplayCustomerLastNameRequest(string newName = "");
+    }
+}
