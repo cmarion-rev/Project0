72fc5094c043518fc8178bdb3be923e64d2fb922
Cameron Marion <cameron.marion@revature.net>
2019-10-21 11:17:25 -0400

    Created new interface for consolidating interface inheritance of all account related methods.

---
+++ Project 0/Project 0/UI/Interfaces/IDisplayAccount.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0.UI.Interfaces
+{
+    interface IDisplayAccount : IDisplayAccountTransactions, IDisplayAccountDeposit, IDisplayAccountWithdraw, IDisplayAccountTransfer, IDisplayAccountLoanPayment
+    {
+    }
+}
