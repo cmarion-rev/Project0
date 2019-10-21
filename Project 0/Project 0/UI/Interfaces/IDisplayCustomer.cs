69d63c44e269060acf315ca65c9ae936345cf90f
Cameron Marion <cameron.marion@revature.net>
2019-10-21 11:24:54 -0400

    Created new interface for consolidating inheritance of display customer interfaces.

---
+++ Project 0/Project 0/UI/Interfaces/IDisplayCustomer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Project_0.CustomerInterfaces;
+
+namespace Project_0
+{
+    interface IDisplayCustomer : IDisplayCustomerAccounts,
+                                 IDisplayCustomerInformation
+    {
+    }
+}
