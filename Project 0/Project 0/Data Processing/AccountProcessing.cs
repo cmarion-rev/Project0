c4dcce00ea90302cbb14cbef0fe7f71a4976d128
Cameron Marion <cameron.marion@revature.net>
2019-10-21 17:56:56 -0400

    Added in functionality for displaying all account for current selected customer.

--- Project 0/Project 0/Data Processing/AccountProcessing.cs
+++ Project 0/Project 0/Data Processing/AccountProcessing.cs
@@ -103,6 +103,7 @@ namespace Project_0
                         break;
 
                     case Utility.OperationState.DISPLAY_ACCOUNTS:
+                        DisplayCustomerAccounts();
                         break;
 
                     case Utility.OperationState.DISPLAY_TRANSACTIONS:
@@ -354,7 +355,7 @@ namespace Project_0
 
         #region REGISTER NEW CUSTOMER METHODS
 
-        public void RegisterNewCustomer()
+        private void RegisterNewCustomer()
         {
             string firstName = "";
             string lastName = "";
@@ -694,6 +695,104 @@ namespace Project_0
 
             // Add new checking account to account storage.
             workingAccountStorage?.AddAccount(activeAccount);
+        }
+
+        #endregion
+
+        #region DISPLAY CUSTOMER ACCOUNTS
+
+        private void DisplayCustomerAccounts()
+        {
+            if (activeCustomer != null)
+            {
+                // Get all accounts for current selected customer.
+                List<Account> allAccounts = activeCustomer.GetAllAccounts();
+
+                // Check if any account exists.
+                if (allAccounts.Count > 0)
+                {
+                    List<CheckingAccount> allCheckingAccounts = new List<CheckingAccount>();
+                    List<BusinessAccount> allBusinessAccounts = new List<BusinessAccount>();
+                    List<TermDepositAccount> allTermAccounts = new List<TermDepositAccount>();
+                    List<LoanAccount> allLoanAccounts = new List<LoanAccount>();
+
+                    // Split appart main account list.
+                    SeperateAccounts(allAccounts, ref allCheckingAccounts, ref allBusinessAccounts, ref allTermAccounts, ref allLoanAccounts);
+
+                    // Display header.
+                    workingDisplay?.ClearDisplay();
+                    workingDisplay?.DisplayCustomerInformation(activeCustomer);
+
+                    // Display all accounts.
+                    DisplayAllAccounts(allCheckingAccounts, allBusinessAccounts, allTermAccounts, allLoanAccounts);
+
+                    // Await user to return to main menu.
+                    workingDisplay?.DisplayReturningToMainMenu();
+                    workingDisplay?.WaitForUserConfirmation();
+                }
+            }
+        }
+
+        private void SeperateAccounts(List<Account> allAccounts,
+                                      ref List<CheckingAccount> allCheckingAccounts,
+                                      ref List<BusinessAccount> allBusinessAccounts,
+                                      ref List<TermDepositAccount> allTermAccounts,
+                                      ref List<LoanAccount> allLoanAccounts)
+        {
+            foreach (IAccountInfo item in allAccounts)
+            {
+                switch (item.AccountType)
+                {
+                    case Utility.AccountType.CHECKING:
+                        allCheckingAccounts.Add(item as CheckingAccount);
+                        break;
+                    
+                    case Utility.AccountType.BUSINESS:
+                        allBusinessAccounts.Add(item as BusinessAccount);
+                        break;
+                    
+                    case Utility.AccountType.TERM:
+                        allTermAccounts.Add(item as TermDepositAccount);
+                        break;
+                    
+                    case Utility.AccountType.LOAN:
+                        allLoanAccounts.Add(item as LoanAccount);
+                        break;
+                 
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void DisplayAllAccounts(List<CheckingAccount> allCheckingAccounts,
+                                       List<BusinessAccount> allBusinessAccounts,
+                                       List<TermDepositAccount> allTermAccounts,
+                                       List<LoanAccount> allLoanAccounts)
+        {
+            // Check if any checking accounts.
+            if (allCheckingAccounts.Count > 0)
+            {
+                workingDisplay?.DisplayAllCustomerAccountsByType(Utility.AccountType.CHECKING, allCheckingAccounts.ToArray());
+            }
+
+            // Check if any business accounts.
+            if (allBusinessAccounts.Count > 0)
+            {
+                workingDisplay?.DisplayAllCustomerAccountsByType(Utility.AccountType.BUSINESS, allBusinessAccounts.ToArray());
+            }
+
+            // Check if any term accounts.
+            if (allTermAccounts.Count > 0)
+            {
+                workingDisplay?.DisplayAllCustomerAccountsByType(Utility.AccountType.TERM, allTermAccounts.ToArray());
+            }
+
+            // Check if any loan accounts.
+            if (allLoanAccounts.Count > 0)
+            {
+                workingDisplay?.DisplayAllCustomerAccountsByType(Utility.AccountType.LOAN, allLoanAccounts.ToArray());
+            }
         }
 
         #endregion
