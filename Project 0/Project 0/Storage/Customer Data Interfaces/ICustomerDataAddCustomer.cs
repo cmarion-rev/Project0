c17cb7eecc97f6e491979b9556a8cbe865fdd3f1
Cameron Marion <cameron.marion@revature.net>
2019-10-21 09:12:42 -0400

    Created new interface for adding customers to Customer Data class.

---
+++ Project 0/Project 0/Storage/Customer Data Interfaces/ICustomerDataAddCustomer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface ICustomerDataAddCustomer
+    {
+        public bool AddCustomer(Customer newCustomer);
+    }
+}
