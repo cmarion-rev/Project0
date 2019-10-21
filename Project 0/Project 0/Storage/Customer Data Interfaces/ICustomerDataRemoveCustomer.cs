7866a94e83d142518eb44f838464b89efa136352
Cameron Marion <cameron.marion@revature.net>
2019-10-21 09:14:13 -0400

    Created interface for deleting customer, by reference or id number, from Customer Data class.

---
+++ Project 0/Project 0/Storage/Customer Data Interfaces/ICustomerDataRemoveCustomer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface ICustomerDataRemoveCustomer
+    {
+        public bool RemoveCustomer(Customer newCustomer);
+
+        public bool RemoveCustomer(int customerID);
+    }
+}
