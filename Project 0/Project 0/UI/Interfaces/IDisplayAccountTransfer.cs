3717f276edfe4c3d561d1dccef2a5df821f30f04
Cameron Marion <cameron.marion@revature.net>
2019-10-21 11:06:30 -0400

    Created new interface for transferring amounts between two accounts.

---
+++ Project 0/Project 0/UI/Interfaces/IDisplayAccountTransfer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    interface IDisplayAccountTransfer
+    {
+        public void DisplayAccountTransferSource(Account[] newAccount);
+        
+        public void DisplayAccountTransferDestination(Account[] newAccount);
+
+        public void DisplayAccountTransfer(Account[] sourceAccount, Account[] destinationAccount);
+    }
+}
