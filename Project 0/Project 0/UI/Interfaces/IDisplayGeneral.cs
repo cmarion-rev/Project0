136a46196bbc5716d9a7a83121b67f6d40209fe9
Cameron Marion <cameron.marion@revature.net>
2019-10-21 11:27:54 -0400

    Created new interface for consolidating inheritance of general display interfaces.

---
+++ Project 0/Project 0/UI/Interfaces/IDisplayGeneral.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Project_0.GeneralInterfaces;
+
+namespace Project_0
+{
+    interface IDisplayGeneral : IDisplayMainMenu, 
+                                IDisplayUserInput, 
+                                IDisplayErrorMessages
+    {
+    }
+}
