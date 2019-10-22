56b3c8b741e0db4ce6f71eebc31e69af5edca47b
Cameron Marion <cameron.marion@revature.net>
2019-10-22 10:58:07 -0400

    Created new interface for messaging user during account closing process.

---
+++ Project 0/Project 0/UI/Interfaces/Display Account Interfaces/IDisplayAccountClose.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0.AccountInterfaces
+{
+    interface IDisplayAccountClose
+    {
+        public void DisplayAccountCloseSelection();
+
+        public void DisplayAccountCloseConfirmation();
+
+        public void DisplayAccountCloseCompleted(int accountNumber);
+    }
+}
