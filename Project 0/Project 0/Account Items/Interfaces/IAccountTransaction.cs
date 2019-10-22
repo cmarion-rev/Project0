cb1a19ab338c32742ca573e5facc85e545885602
Cameron Marion <cameron.marion@revature.net>
2019-10-22 09:45:12 -0400

    Created new interface for accessing account transaction records.

---
+++ Project 0/Project 0/Account Items/Interfaces/IAccountTransaction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    public interface IAccountTransaction
+    {
+        public ITransactionRecord[] GetTransactionRecords();
+    }
+}
