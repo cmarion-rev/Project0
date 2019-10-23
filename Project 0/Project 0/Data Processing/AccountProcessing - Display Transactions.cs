ce110aa4fa3ed474d93bb4d77c2c4a562a649adb
Cameron Marion <cameron.marion@revature.net>
2019-10-23 09:31:46 -0400

    Moved all display transactions functions to new partial class file.

---
+++ Project 0/Project 0/Data Processing/AccountProcessing - Display Transactions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    partial class AccountProcessing
+    {
+        private void DisplayTransactionsForAccount()
+        {
+            if (activeCustomer != null)
+            {
+                // Get all accounts for current selected customer.
+                List<Account> allAccounts = activeCustomer.GetAllAccounts();
+
+                // Check if any account exists.
+                if (allAccounts.Count > 0)
+                {
+                    List<CheckingAccount> allCheckingAccounts = new List<CheckingAccount>();
+                    List<BusinessAccount> allBusinessAccounts = new List<BusinessAccount>();
+                    List<TermDepositAccount> allTermAccounts = new List<TermDepositAccount>();
+                    List<LoanAccount> allLoanAccounts = new List<LoanAccount>();
+
+                    // Split appart main account list.
+                    Utility.SeperateAccounts(allAccounts, ref allCheckingAccounts, ref allBusinessAccounts, ref allTermAccounts, ref allLoanAccounts);
+
+                    // Display header.
+                    workingDisplay?.ClearDisplay();
+                    workingDisplay?.DisplayCustomerInformation(activeCustomer);
+
+                    // Process user input selection.
+                    ProcessAccountForTransactionDisplay(allAccounts, allCheckingAccounts, allBusinessAccounts, allTermAccounts, allLoanAccounts);
+
+                    // Await user to return to main menu.
+                    workingDisplay?.DisplayReturningToMainMenu();
+                    workingDisplay?.WaitForUserConfirmation();
+                }
+            }
+        }
+
+        private void ProcessAccountForTransactionDisplay(List<Account> allAccounts,
+                                                         List<CheckingAccount> allCheckingAccounts,
+                                                         List<BusinessAccount> allBusinessAccounts,
+                                                         List<TermDepositAccount> allTermAccounts,
+                                                         List<LoanAccount> allLoanAccounts)
+        {
+            int? accountID = -1;
+            DisplayAllAccounts(allCheckingAccounts, allBusinessAccounts, allTermAccounts, allLoanAccounts);
+            workingDisplay?.DisplayAccountTransactionSelection();
+            accountID = workingDisplay?.GetUserOptionNumberSelection();
+
+            // Check if account selected is in current list.
+            if (accountID != null)
+            {
+                bool isFound = false;
+                int actualID = accountID.GetValueOrDefault(-1);
+                foreach (IAccountInfo currentAccount in allAccounts)
+                {
+                    if (currentAccount.AccountNumber == actualID)
+                    {
+                        activeAccount = (currentAccount as Account);
+                        DisplayAccountTransactions();
+                        isFound = true;
+                        break;
+                    }
+                }
+
+                if (!isFound)
+                {
+                    workingDisplay?.DisplayInvalidSelection();
+                }
+            }
+        }
+
+        private void DisplayAccountTransactions()
+        {
+            if (activeAccount != null)
+            {
+                // Display header
+                workingDisplay?.ClearDisplay();
+                workingDisplay?.DisplayCustomerInformation(activeCustomer);
+                workingDisplay?.DisplayAccountInfo(activeAccount as IAccountInfo);
+
+                // Display records.
+                workingDisplay.DisplayAllAccountTransactions(activeAccount.GetTransactionRecords());
+
+                activeAccount = null;
+            }
+        }
+    }
+}
