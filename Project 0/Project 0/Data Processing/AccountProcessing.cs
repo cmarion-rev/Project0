2b7d9acc47e7d9b985558c8d318d98782ddb8fcb
Cameron Marion <cameron.marion@revature.net>
2019-10-23 09:27:09 -0400

    Moved all display accounts functions to new partial class file.

--- Project 0/Project 0/Data Processing/AccountProcessing.cs
+++ Project 0/Project 0/Data Processing/AccountProcessing.cs
@@ -134,78 +134,11 @@ namespace Project_0
             return isGameLoopActive;
         }
 
-        #region TRANSFER BETWEEN ACCOUNTS METHODS
-
-        
-        #endregion
-
         #region PAY LOAN INSTALLMENT METHODS
 
         #endregion
 
         #region DISPLAY CUSTOMER ACCOUNTS METHODS
-
-        private void DisplayCustomerAccounts()
-        {
-            if (activeCustomer != null)
-            {
-                // Get all accounts for current selected customer.
-                List<Account> allAccounts = activeCustomer.GetAllAccounts();
-
-                // Check if any account exists.
-                if (allAccounts.Count > 0)
-                {
-                    List<CheckingAccount> allCheckingAccounts = new List<CheckingAccount>();
-                    List<BusinessAccount> allBusinessAccounts = new List<BusinessAccount>();
-                    List<TermDepositAccount> allTermAccounts = new List<TermDepositAccount>();
-                    List<LoanAccount> allLoanAccounts = new List<LoanAccount>();
-
-                    // Split appart main account list.
-                    Utility.SeperateAccounts(allAccounts, ref allCheckingAccounts, ref allBusinessAccounts, ref allTermAccounts, ref allLoanAccounts);
-
-                    // Display header.
-                    workingDisplay?.ClearDisplay();
-                    workingDisplay?.DisplayCustomerInformation(activeCustomer);
-
-                    // Display all accounts.
-                    DisplayAllAccounts(allCheckingAccounts, allBusinessAccounts, allTermAccounts, allLoanAccounts);
-
-                    // Await user to return to main menu.
-                    workingDisplay?.DisplayReturningToMainMenu();
-                    workingDisplay?.WaitForUserConfirmation();
-                }
-            }
-        }
-
-        private void DisplayAllAccounts(List<CheckingAccount> allCheckingAccounts,
-                                        List<BusinessAccount> allBusinessAccounts,
-                                        List<TermDepositAccount> allTermAccounts,
-                                        List<LoanAccount> allLoanAccounts)
-        {
-            // Check if any checking accounts.
-            if (allCheckingAccounts.Count > 0)
-            {
-                workingDisplay?.DisplayAllCustomerAccountsByType(Utility.AccountType.CHECKING, allCheckingAccounts.ToArray());
-            }
-
-            // Check if any business accounts.
-            if (allBusinessAccounts.Count > 0)
-            {
-                workingDisplay?.DisplayAllCustomerAccountsByType(Utility.AccountType.BUSINESS, allBusinessAccounts.ToArray());
-            }
-
-            // Check if any term accounts.
-            if (allTermAccounts.Count > 0)
-            {
-                workingDisplay?.DisplayAllCustomerAccountsByType(Utility.AccountType.TERM, allTermAccounts.ToArray());
-            }
-
-            // Check if any loan accounts.
-            if (allLoanAccounts.Count > 0)
-            {
-                workingDisplay?.DisplayAllCustomerAccountsByType(Utility.AccountType.LOAN, allLoanAccounts.ToArray());
-            }
-        }
 
         #endregion
 
