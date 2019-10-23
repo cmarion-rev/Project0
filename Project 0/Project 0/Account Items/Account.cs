725ca2d5ecf486d9fbca84b69bdd9353d638cf2e
Cameron Marion <cameron.marion@revature.net>
2019-10-23 15:48:26 -0400

    Started rework of Account abstract class by integrating abstraction of IAccountInfo interface.

--- Project 0/Project 0/Account Items/Account.cs
+++ Project 0/Project 0/Account Items/Account.cs
@@ -4,18 +4,34 @@ using System.Text;
 
 namespace Project_0
 {
-    public abstract class Account: IAccountTransaction
+    public abstract class Account: IAccountTransaction, IAccountInfo
     {
         protected List<ITransactionRecord> totalRecords = new List<ITransactionRecord>();
+
+        public abstract Utility.AccountType AccountType { get; }
         
         public Account()
         {
             totalRecords = new List<ITransactionRecord>();
         }
+        
+        public abstract int AccountNumber { get; }
+        
+        public abstract int CustomerID { get; set; }
+        
+        public abstract Customer Customer { get; set; }
+        
+        public abstract double AccountBalance { get; protected set; }
+        
+        public abstract Utility.TransactionErrorCodes LastTransactionState { get; protected set; }
 
         public ITransactionRecord[] GetTransactionRecords()
         {
             return totalRecords.ToArray();
         }
+
+        public abstract bool DepositAmount(double newAmount);
+        
+        public abstract bool WithdrawAmount(double newAmount);
     }
 }
