3b3c77befd2c63e2920a6bef4a5cae403ac239e2
Cameron Marion <cameron.marion@revature.net>
2019-10-23 20:41:33 -0400

    Minor code cleanup in Account abstract class.

--- Project 0/Project 0/Account Items/Account.cs
+++ Project 0/Project 0/Account Items/Account.cs
@@ -4,23 +4,23 @@ using System.Text;
 
 namespace Project_0
 {
-    public abstract class Account: IAccountTransaction, IAccountInfo
+    public abstract class Account : IAccountTransaction, IAccountInfo
     {
         protected List<ITransactionRecord> totalRecords = new List<ITransactionRecord>();
 
         public abstract Utility.AccountType AccountType { get; }
-        
+
         public Account()
         {
             totalRecords = new List<ITransactionRecord>();
         }
-        
+
         public abstract int AccountNumber { get; }
-        
+
         public abstract int CustomerID { get; set; }
-        
+
         public abstract Customer Customer { get; set; }
-        
+
         public abstract double AccountBalance { get; protected set; }
 
         public abstract Utility.TransactionErrorCodes LastTransactionState { get; protected set; }
@@ -33,7 +33,7 @@ namespace Project_0
         }
 
         public abstract bool DepositAmount(double newAmount);
-        
+
         public abstract bool WithdrawAmount(double newAmount);
     }
 }
