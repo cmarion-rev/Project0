9e864d2cf74dfd3606fa9696d80aef64a4c86c5e
Cameron Marion <cameron.marion@revature.net>
2019-10-24 11:23:10 -0400

    Moved customer selection messages to new partial class file Display - Customer Select.

--- Project 0/Project 0/UI/Display.cs
+++ Project 0/Project 0/UI/Display.cs
@@ -25,34 +25,5 @@ namespace Project_0
         {
 
         }
-
-        #region CUSTOMER OPTIONS
-
-     
-      
-
-        public void DisplayCustomerList(Customer[] allCustomers, Customer currentCustomer)
-        {
-            Console.Clear();
-
-            // Display current customer.
-            Console.WriteLine("Current Customer");
-            Console.WriteLine("Customer: {0}", currentCustomer.FullName);
-            Console.WriteLine("Customer ID: {0}", currentCustomer.CustomerID);
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Customer ID#\t:\tCustomer Name");
-
-            // Display list of other customers.
-            foreach (Customer otherCustomer in allCustomers)
-            {
-                Console.WriteLine("{0,12}\t \t{1} {2}", otherCustomer.CustomerID, otherCustomer.FirstName, otherCustomer.LastName);
-            }
-
-            Console.WriteLine();
-            Console.Write("Please select a customer ID: ");
-        }
-
-        #endregion
     }
 }
