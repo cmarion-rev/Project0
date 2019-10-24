c6d5be70b9ce4dd326a3aca844eacb4ffb9f7b50
Cameron Marion <cameron.marion@revature.net>
2019-10-24 10:54:35 -0400

    Moved menu messages to new partial class file Display - Menus.

--- Project 0/Project 0/UI/Display.cs
+++ Project 0/Project 0/UI/Display.cs
@@ -26,63 +26,7 @@ namespace Project_0
 
         }
 
-        #region ERROR MESSAGES
-
-     
-
-        #endregion
-
         #region MENUS
-
-        public void DisplayMainMenu(Utility.MainMenuOptions newOptions)
-        {
-            // Check if Register New Customer option is set.
-            Console.WriteLine((newOptions & Utility.MainMenuOptions.REGISTER_NEW_CUSTOMER) == Utility.MainMenuOptions.REGISTER_NEW_CUSTOMER ? "(1) Register new customer." : "");
-
-            // Check if Change Customer option is set.
-            Console.WriteLine((newOptions & Utility.MainMenuOptions.CHANGE_CUSTOMER) == Utility.MainMenuOptions.CHANGE_CUSTOMER ? "(2) Change customer." : "");
-
-            // Check if Open New Account option is set.
-            Console.WriteLine((newOptions & Utility.MainMenuOptions.OPEN_NEW_ACCOUNT) == Utility.MainMenuOptions.OPEN_NEW_ACCOUNT ? "(3) Open new account." : "");
-
-            // Check if Close Account option is set.
-            Console.WriteLine((newOptions & Utility.MainMenuOptions.CLOSE_ACCOUNT) == Utility.MainMenuOptions.CLOSE_ACCOUNT ? "(4) Close account." : "");
-
-            // Check if Deposit Amount option is set.
-            Console.WriteLine((newOptions & Utility.MainMenuOptions.DEPOSIT_AMOUNT) == Utility.MainMenuOptions.DEPOSIT_AMOUNT ? "(5) Deposit to account." : "");
-
-            // Check if Withdraw Amount option is set.
-            Console.WriteLine((newOptions & Utility.MainMenuOptions.WITHDRAW_AMOUNT) == Utility.MainMenuOptions.WITHDRAW_AMOUNT ? "(6) Withdraw from account." : "");
-
-            // Check if Transfer Amount option is set.
-            Console.WriteLine((newOptions & Utility.MainMenuOptions.TRANSFER_AMOUNT) == Utility.MainMenuOptions.TRANSFER_AMOUNT ? "(7) Transfer between accounts." : "");
-
-            // Check if Pay Loan Installment option is set.
-            Console.WriteLine((newOptions & Utility.MainMenuOptions.PAY_LOAN_INSTALLMENT) == Utility.MainMenuOptions.PAY_LOAN_INSTALLMENT ? "(8) Pay loan installment." : "");
-
-            // Check if Display Accounts option is set.
-            Console.WriteLine((newOptions & Utility.MainMenuOptions.DISPLAY_ALL_ACCOUNTS) == Utility.MainMenuOptions.DISPLAY_ALL_ACCOUNTS ? "(9) Display all accounts." : "");
-
-            // Check if Display Transactions option is set.
-            Console.WriteLine((newOptions & Utility.MainMenuOptions.DISPLAY_ALL_TRANSACTIONS) == Utility.MainMenuOptions.DISPLAY_ALL_TRANSACTIONS ? "(0) Display all transactions for an account." : "");
-
-            // Display exit program option.
-            Console.WriteLine();
-            Console.WriteLine("(Q) Exit Program.");
-            Console.WriteLine();
-            Console.WriteLine();
-        }
-
-        public void DisplayReturningToMainMenu()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Returning to Main Menu.");
-        }
-
-        public void ClearDisplay()
-        {
-            Console.Clear();
-        }
 
         #endregion
 
