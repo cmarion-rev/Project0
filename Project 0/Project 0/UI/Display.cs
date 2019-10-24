511de25b81c41ee957d234984556c404607d118b
Cameron Marion <cameron.marion@revature.net>
2019-10-24 11:12:46 -0400

    Moved account transfer messages to new partial class file Display - Account Transfer.

--- Project 0/Project 0/UI/Display.cs
+++ Project 0/Project 0/UI/Display.cs
@@ -28,50 +28,8 @@ namespace Project_0
 
         #region ACCOUNT OPTIONS
 
-        #region ACCOUNT WITHDRAWAL OPTIONS
-
-       
-
-        #endregion
-
         #region ACCOUNT TRANSFER OPTIONS
-
-        public void DisplayTransferSourceAccount(Account[] allAccounts)
-        {
-            DisplayAllCustomerAccounts(allAccounts);
-
-            Console.WriteLine();
-            Console.Write("Please enter account number to transfer from: ");
-        }
-
-        public void DisplayTransferDestinationAccount(Account[] allAccounts)
-        {
-            DisplayAllCustomerAccounts(allAccounts);
-
-            Console.WriteLine();
-            Console.Write("Please enter account number to transfer to: ");
-        }
-
-        public void DisplayAccountTransfer(Account sourceAccount, Account destinationAccount)
-        {
-            Console.WriteLine("Source Account");
-            DisplayAccountInfo(sourceAccount);
-
-            Console.WriteLine();
-
-            Console.WriteLine("Destination Account");
-            DisplayAccountInfo(destinationAccount);
-
-            Console.WriteLine();
-            Console.Write("Please enter amount to transfer: ");
-        }
-
-        public void DisplayTransferSuccessful(Account sourceAccount, Account destinationAccount)
-        {
-            Console.WriteLine();
-            Console.WriteLine("Transfer from account #{0} to account #{1} successful.", sourceAccount.AccountNumber, destinationAccount.AccountNumber);
-        }
-
+       
         #endregion
 
         #region LOAN ACCOUNT OPTIONS
---
+++ Project 0/Project 0/UI/Partial/Account/Display - Account Transfer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    partial class Display : IDisplayGeneral, IDisplayAccount, IDisplayCustomer
+    {
+        public void DisplayTransferSourceAccount(Account[] allAccounts)
+        {
+            DisplayAllCustomerAccounts(allAccounts);
+
+            Console.WriteLine();
+            Console.Write("Please enter account number to transfer from: ");
+        }
+
+        public void DisplayTransferDestinationAccount(Account[] allAccounts)
+        {
+            DisplayAllCustomerAccounts(allAccounts);
+
+            Console.WriteLine();
+            Console.Write("Please enter account number to transfer to: ");
+        }
+
+        public void DisplayAccountTransfer(Account sourceAccount, Account destinationAccount)
+        {
+            Console.WriteLine("Source Account");
+            DisplayAccountInfo(sourceAccount);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Destination Account");
+            DisplayAccountInfo(destinationAccount);
+
+            Console.WriteLine();
+            Console.Write("Please enter amount to transfer: ");
+        }
+
+        public void DisplayTransferSuccessful(Account sourceAccount, Account destinationAccount)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Transfer from account #{0} to account #{1} successful.", sourceAccount.AccountNumber, destinationAccount.AccountNumber);
+        }
+    }
+}
