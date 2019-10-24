0c6acd01d31d519c64b1e1ea20d153f8003956d1
Cameron Marion <cameron.marion@revature.net>
2019-10-24 11:21:24 -0400

    Moved new customer message to new partial class file Display - Customer Create.

--- Project 0/Project 0/UI/Display.cs
+++ Project 0/Project 0/UI/Display.cs
@@ -29,34 +29,7 @@ namespace Project_0
         #region CUSTOMER OPTIONS
 
      
-        public void DisplayNewCustomerScreen()
-        {
-            Console.WriteLine("New Customer");
-        }
-
-        public void DisplayCustomerFirstNameRequest(string newName = "")
-        {
-            if (newName.Length > 0)
-            {
-                Console.WriteLine("First Name: {0}", newName);
-            }
-            else
-            {
-                Console.Write("First Name: ");
-            }
-        }
-
-        public void DisplayCustomerLastNameRequest(string newName = "")
-        {
-            if (newName.Length > 0)
-            {
-                Console.WriteLine("Last Name: {0}", newName);
-            }
-            else
-            {
-                Console.Write("Last Name: ");
-            }
-        }
+      
 
         public void DisplayCustomerList(Customer[] allCustomers, Customer currentCustomer)
         {
---
+++ Project 0/Project 0/UI/Partial/Customer/Display - Customer Create.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0
+{
+    partial class Display : IDisplayGeneral, IDisplayAccount, IDisplayCustomer
+    {
+        public void DisplayNewCustomerScreen()
+        {
+            Console.WriteLine("New Customer");
+        }
+
+        public void DisplayCustomerFirstNameRequest(string newName = "")
+        {
+            if (newName.Length > 0)
+            {
+                Console.WriteLine("First Name: {0}", newName);
+            }
+            else
+            {
+                Console.Write("First Name: ");
+            }
+        }
+
+        public void DisplayCustomerLastNameRequest(string newName = "")
+        {
+            if (newName.Length > 0)
+            {
+                Console.WriteLine("Last Name: {0}", newName);
+            }
+            else
+            {
+                Console.Write("Last Name: ");
+            }
+        }
+    }
+}
