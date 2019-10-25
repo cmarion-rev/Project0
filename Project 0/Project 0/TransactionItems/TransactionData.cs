46c88f5c416bf8df073511c3e9c197cb2cefbfe4
Cameron Marion <cameron.marion@revature.net>
2019-10-25 10:44:13 -0400

    Created new TransactionData class for future ORM database interactions.

---
+++ Project 0/Project 0/TransactionItems/TransactionData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0.TransactionItems
+{
+    class TransactionData
+    {
+        public int ID { get; set; }
+
+        public int SourceAccountID { get; set; }
+
+        public int DestinationAccountID { get; set; }
+
+        public double Amount { get; set; }
+
+        public int TransactionCode { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+    }
+}
