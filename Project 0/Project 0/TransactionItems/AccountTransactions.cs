9cdd484b79fe5248239d9c8abe0f8369e37ede47
Cameron Marion <cameron.marion@revature.net>
2019-10-28 13:30:15 -0400

    Created new class for future implementation of AccountTransactions relationship.

---
+++ Project 0/Project 0/TransactionItems/AccountTransactions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0.TransactionItems
+{
+    class AccountTransactions
+    {
+        public int AccountID { get; set; }
+
+        public int TransactionID { get; set; }
+    }
+}
