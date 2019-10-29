3d8d426b940ec6c467eccdd4f0eba7a6df0e39d2
Cameron Marion <cameron.marion@revature.net>
2019-10-29 12:31:14 -0400

    Added in more comments to CustomerStorage class.

--- Project 0/Project 0/Storage/CustomerStorage.cs
+++ Project 0/Project 0/Storage/CustomerStorage.cs
@@ -27,6 +27,12 @@ namespace Project_0
             allCustomer = new List<Customer>();
         }
 
+        /// <summary>
+        /// Create new customer.
+        /// </summary>
+        /// <param name="firstName">Customer first name.</param>
+        /// <param name="lastName">Customer last name.</param>
+        /// <returns>Returns new Customer object.</returns>
         public Customer AddCustomer(string firstName, string lastName)
         {
             Customer newCustomer = null;
@@ -48,10 +54,16 @@ namespace Project_0
             return newCustomer;
         }
 
+        /// <summary>
+        /// Get customer by ID.
+        /// </summary>
+        /// <param name="customerID">ID of customer.</param>
+        /// <returns>Returns Customer object reference.</returns>
         public Customer GetCustomer(int customerID)
         {
             Customer result = null;
 
+            // Check if ID is within valid range.
             if (customerID > -1)
             {
                 if (customerID < allCustomer.Count)
@@ -63,26 +75,39 @@ namespace Project_0
             return result;
         }
 
+        /// <summary>
+        /// Get all customers.
+        /// </summary>
+        /// <returns>Returns an array of all customer objects currently in storage.</returns>
         public Customer[] GetAllCustomers()
         {
             return allCustomer.ToArray();
         }
 
+        /// <summary>
+        /// Remove customer from storage.
+        /// </summary>
+        /// <param name="newCustomer">Reference customer object.</param>
+        /// <returns>Returns, True if customer was properly removed from storage. Otherwise, False.</returns>
         public bool RemoveCustomer(Customer newCustomer)
         {
             bool result = true;
+
+            // Get list index of referenced customer object.
             int customerIndex = allCustomer.IndexOf(newCustomer);
            
+            // Check if index value is valid.
             if (customerIndex > -1)
             {
                 // Remove all relevant accounts from current customer.
                 List<Account> allCustomerAccounts = newCustomer.GetAllAccounts();
                 if (allCustomerAccounts.Count > 0)
                 {
-                    AccountStorage activeAccounts = AccountStorage.Instance;
+                    // Remove all customer accounts from storage.
+                    AccountStorage activeAccountStorage = AccountStorage.Instance;
                     foreach (Account item in allCustomerAccounts)
                     {
-                        activeAccounts.RemoveAccount(item);
+                        activeAccountStorage?.RemoveAccount(item);
                     }
                 }
 
@@ -91,21 +116,31 @@ namespace Project_0
             }
             else
             {
+                // Customer not present in storage.
                 result = false;
             }
 
             return result;
         }
 
+        /// <summary>
+        /// Remove customer from storage.
+        /// </summary>
+        /// <param name="customerID">ID of customer to remove.</param>
+        /// <returns>Returns, True if customer was properly removed from storage. Otherwise, False.</returns>
         public bool RemoveCustomer(int customerID)
         {
-            bool result = true;
+            bool result = false;
 
+            // Check if ID is within valid range.
             if (customerID > -1)
             {
                 if (customerID < allCustomer.Count)
                 {
+                    // Get reference of specific customer.
                     Customer currentCustomer = allCustomer[customerID];
+
+                    // Remove customer object from storage.
                     result = RemoveCustomer(currentCustomer);
                 }
                 else
@@ -121,6 +156,10 @@ namespace Project_0
             return result;
         }
 
+        /// <summary>
+        /// Generate new CustomerData object.
+        /// </summary>
+        /// <returns>Returns a new CustomerData property object.</returns>
         private CustomerData GenerateNewCustomerData()
         {
             CustomerData newData = null;
